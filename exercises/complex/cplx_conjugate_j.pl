f13cd262834afd89bec74110dd99f3ed0ce3f95b
dr <dr@pl-test.u-pem.fr>
2019-02-08 22:56:07 +0100

    Update cplx_conjugate_j.pl

--- exercises/complex/cplx_conjugate_j.pl
+++ exercises/complex/cplx_conjugate_j.pl
@@ -2,10 +2,6 @@ extends = /exercises/complex/cplx_conjugate.pl
 
 title = Conjugué d'un nombre complexe (notation j)
 
-builder_param ==
-ut.LatexPrinter._settings.update({'imaginary_unit': 'j'})
-==
+latexparam = {'imaginary_unit': 'j'}
 
-eval_param ==
-imaginary_unit='j'
-==
+imaginary_unit = j
