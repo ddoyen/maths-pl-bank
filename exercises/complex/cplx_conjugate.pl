ac672aae459c8f375001acf1f8075ece0c5e11e8
dr <dr@pl-test.u-pem.fr>
2019-02-08 22:49:30 +0100

    Update cplx_conjugate.pl

--- exercises/complex/cplx_conjugate.pl
+++ exercises/complex/cplx_conjugate.pl
@@ -2,22 +2,20 @@ extends = /exercises/templates/basicmath.pl
 
 title = Conjugué d'un nombre complexe
 
-builder_main ==
+lang = fr
+
+imaginary_unit= i
+
+before ==
 z=ut.rand_complex_int(5)
+latexz=ut.latex(z)
 sol=sp.conjugate(z)
-strsol=str(sol)
 ==
 
-builder_statement ==
-text = """Quel est le conjugué du nombre complexe $%z ={}%$ ?""".format(ut.latex(z))
+text = Quel est le conjugué du nombre complexe $%z ={{latexz}}%$ ?
+
+evaluator ==
+score,numerror,texterror=ut.ans_complex(answer['1'],sol,imaginary_unit)
+feedback=fb.msg_analysis(score,texterror,lang)
 ==
 
-eval_param ==
-imaginary_unit='i'
-==
-
-eval_main==
-sol=sp.sympify(strsol)
-score,numerror,texterror=ut.ans_complex(response['answer'],sol,imaginary_unit)
-feedback=texterror
-==
